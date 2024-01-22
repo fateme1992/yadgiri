3006d5d476e87b9cfbc667746cd508f6432f74db
fatemeh <amoralashkar@gmail.com>
2024-01-22 13:30:45 +0330

    add new

--- projeh/prj/views.py
+++ projeh/prj/views.py
@@ -3,12 +3,16 @@ from . import models
 from .models import post
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 class postListView(ListView):
     queryset = post.published.all()
     context_object_name = "post"
     peginate_by=4
     template_name = 'post/list.html'
+
+
 # Create your views here.
 #def post_list(request):
     #posts = post.published.all()
@@ -28,5 +32,27 @@ class postListView(ListView):
 def post_detail(request,year,month,day,post):
     post= get_object_or_404(post, slug=post, status="published", publish__year=year, publish__month=month,publish__day=day)
     return render(request, 'post/detail.html', {"post": post})
+
+def post_share(request,post_id):
+    post =get_object_or_404(post,id=post_id,status="published")
+    
+    if request.method =='POST':
+        form=EmailPostForm(request.POST)
+        if form.is_valid():
+            cd =form.cleaned_date
+            post_url=request.build_absolute_uri(post.get_absolute_url())
+            subject=f"{cd['name']} recommend you "
+            massage=f"{post.title}in {post_url} and {cd['comment']}"
+            send_mail(
+                subject,massage,"amoralashkar@gmail.com",
+                [cd['to']]
+            )
+            
+    else:
+        form=EmailPostForm()
+        return render(request,'prj/post/share.html',{"post":post,"form":form})
+       
+    
+    
 
 
--- projeh/projeh/settings.py
+++ projeh/projeh/settings.py
@@ -118,6 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Email setting
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER="amoralashkar@gmail.com"
+EMAIL_HOST_PASWWORD="13711371"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
