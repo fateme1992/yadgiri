9cff66107bfcd35fc955e4359be0fcd6fdd675e8
fatemeh <amoralashkar@gmail.com>
2024-01-26 23:56:30 +0330

    add new

--- projeh/projeh/urls.py
+++ projeh/projeh/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('prj/',include('prj.urls'))
+    path('',views.hi),
+    path('prj/',include('prj.urls')),
 ]
+ 
